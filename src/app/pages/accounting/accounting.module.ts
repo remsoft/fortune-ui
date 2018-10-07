e892f9510d289e7158cb84195a853c16b835cde1
Rizky <rizky.akbar.sani@gmail.com>
2018-10-07 08:43:24 +0800

    change module structure

--- src/app/pages/accounting/accounting.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {ThemeModule} from '../../@theme/theme.module';
-import {AccountingRoutingModule,routedComponents} from './accounting-routing.module';
-
-@NgModule({
-    imports:[ThemeModule, AccountingRoutingModule],
-    declarations: [...routedComponents]
-})
-export class AccountingModule{}
